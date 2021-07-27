3891a42fa6618ca402783be01a05ee7326f69a9e
Justin Fyfe <justin@fyfesoftware.ca>
2021-07-27 13:39:48 -0400

    Fixed issue with source effective version

--- SanteDB.Persistence.MDM/Services/Resources/MdmEntityMerger.cs
+++ SanteDB.Persistence.MDM/Services/Resources/MdmEntityMerger.cs
@@ -221,6 +221,7 @@ namespace SanteDB.Persistence.MDM.Services.Resources
                         transactionBundle.AddRange(
                             victim.LoadCollection(o => o.Identifiers).Where(i => !survivor.LoadCollection(o => o.Identifiers).Any(e => e.SemanticEquals(i))).Select(o => new EntityIdentifier(o.Authority, o.Value)
                             {
+                                EffectiveVersionSequenceId = o.EffectiveVersionSequenceId,
                                 SourceEntityKey = survivor.Key,
                                 IssueDate = o.IssueDate,
                                 ExpiryDate = o.ExpiryDate
