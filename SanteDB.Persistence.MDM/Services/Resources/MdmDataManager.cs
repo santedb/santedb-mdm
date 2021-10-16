69a4ecd9eac332da172269d8d850e6a2c34df9e7
Justin Fyfe <justin@fyfesoftware.ca>
2021-10-16 17:41:05 -0400

    Updated for tests for merging on UI

--- SanteDB.Persistence.MDM/Services/Resources/MdmDataManager.cs
+++ SanteDB.Persistence.MDM/Services/Resources/MdmDataManager.cs
@@ -2,22 +2,23 @@
  * Copyright (C) 2021 - 2021, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
  * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
  * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: fyfej
  * Date: 2021-8-5
  */
+
 using SanteDB.Core;
 using SanteDB.Core.BusinessRules;
 using SanteDB.Core.Configuration;
@@ -40,67 +41,14 @@ using System.Text;
 namespace SanteDB.Persistence.MDM.Services.Resources
 {
     /// <summary>
-    /// Represents a data manager which actually interacts with the underlying repository
+    /// A non-generic data manager
     /// </summary>
-    public abstract class MdmDataManager<TModel>
-        where TModel : IdentifiedData
+    public abstract class MdmDataManager
     {
-
-        /// <summary>
-        /// Persistence service
-        /// </summary>
-        protected IDataPersistenceService m_underlyingTypePersistence;
-
-        /// <summary>
-        /// Create a new manager base
-        /// </summary>
-        internal MdmDataManager(IDataPersistenceService underlyingDataPersistence)
-        {
-            if (underlyingDataPersistence == null)
-                throw new InvalidOperationException($"Type {typeof(TModel).FullName} does not have a persistence service registered");
-            this.m_underlyingTypePersistence = underlyingDataPersistence;
-        }
-
-        /// <summary>
-        /// Gets or creates a writable target object for the specified principal
-        /// </summary>
-        public abstract TModel GetLocalFor(Guid dataKey, IPrincipal principal);
-
-
         /// <summary>
         /// Determine if the object is a master
         /// </summary>
         public abstract bool IsMaster(Guid dataKey);
-
-        /// <summary>
-        /// Determine if the object is a master
-        /// </summary>
-        public abstract bool IsMaster(TModel data);
-
-        /// <summary>
-        /// Create local for the specified principal
-        /// </summary>
-        public abstract TModel CreateLocalFor(TModel masterRecord);
-
-        /// <summary>
-        /// Determine if the record is a ROT
-        /// </summary>
-        public abstract bool IsRecordOfTruth(TModel data);
-
-        /// <summary>
-        /// Converts the <paramref name="local"/> to a ROT local
-        /// </summary>
-        public abstract TModel PromoteRecordOfTruth(TModel local);
-
-        /// <summary>
-        /// Get the master for the specified local
-        /// </summary>
-        public abstract IdentifiedData GetRawMaster(TModel local);
-
-        /// <summary>
-        /// Extracts the transactional components which might be in <paramref name="store"/> and return them
-        /// </summary>
-        public abstract IEnumerable<ISimpleAssociation> ExtractRelationships(TModel store);
 
         /// <summary>
         /// Refactor relationships
@@ -113,30 +61,12 @@ namespace SanteDB.Persistence.MDM.Services.Resources
         public abstract IEnumerable<ITargetedAssociation> GetAllMdmCandidateLocals();
 
         /// <summary>
-        /// Validate the MDM state
-        /// </summary>
-        public abstract IEnumerable<DetectedIssue> ValidateMdmState(TModel data);
-
-        /// <summary>
-        /// Synthesize the query 
-        /// </summary>
-        public abstract IEnumerable<IMdmMaster> MdmQuery(NameValueCollection query, NameValueCollection localQuery, Guid? queryId, int offset, int? count, out int totalResults, IEnumerable<ModelSort<TModel>> orderBy);
-
-        /// <summary>
-        /// Gets the raw object from the underlying persistence service identified by the key (whether it is MASTER ENTITY or LOCAL or SYNTH)
-        /// </summary>
-        public object GetRaw(Guid key)
-        {
-            return this.m_underlyingTypePersistence.Get(key);
-        }
-
-        /// <summary>
         /// Gets all local associations between <paramref name="masterKey"/> and its master
         /// </summary>
         public abstract IEnumerable<ITargetedAssociation> GetAssociatedLocals(Guid masterKey);
 
         /// <summary>
-        /// Get all candidate associations between <paramref name="masterKey"/> 
+        /// Get all candidate associations between <paramref name="masterKey"/>
         /// </summary>
         public abstract IEnumerable<ITargetedAssociation> GetCandidateLocals(Guid masterKey);
 
@@ -166,33 +96,7 @@ namespace SanteDB.Persistence.MDM.Services.Resources
         public abstract IMdmMaster GetMasterFor(Guid localKey);
 
         /// <summary>
-        /// Ensures that <paramref name="principal"/> has access to a local <paramref name="data"/>
-        /// </summary>
-        public abstract bool IsOwner(TModel data, IPrincipal principal);
-
-        /// <summary>
-        /// Perform an MDM obsolete operation
-        /// </summary>
-        /// <returns>Whether the obsolete operation resulted in cascading obsolete to the master</returns>
-        public abstract IEnumerable<IdentifiedData> MdmTxObsolete(TModel data, IEnumerable<IdentifiedData> context);
-
-        /// <summary>
-        /// Create a transaction instructions which update <paramref name="data"/> to be the ROT
-        /// </summary>
-        public abstract IEnumerable<IdentifiedData> MdmTxSaveRecordOfTruth(TModel data, IEnumerable<IdentifiedData> context);
-
-        /// <summary>
-        /// Create transaction instructions which update the <paramref name="data"/> as a local 
-        /// </summary>
-        public abstract IEnumerable<IdentifiedData> MdmTxSaveLocal(TModel data, IEnumerable<IdentifiedData> context);
-
-        /// <summary>
-        /// Create transaction instructions which update <paramref name="local"/> to match all masters
-        /// </summary>
-        public abstract IEnumerable<IdentifiedData> MdmTxMatchMasters(TModel local, IEnumerable<IdentifiedData> context);
-
-        /// <summary>
-        /// Merge two master records together 
+        /// Merge two master records together
         /// </summary>
         public abstract IEnumerable<IdentifiedData> MdmTxMergeMasters(Guid survivorKey, Guid victimKey, IEnumerable<IdentifiedData> context);
 
@@ -210,6 +114,107 @@ namespace SanteDB.Persistence.MDM.Services.Resources
         /// Create transaction instructions to unlink a master
         /// </summary>
         public abstract IEnumerable<IdentifiedData> MdmTxMasterUnlink(Guid fromKey, Guid toKey, IEnumerable<IdentifiedData> context);
+    }
+
+    /// <summary>
+    /// Represents a data manager which actually interacts with the underlying repository
+    /// </summary>
+    public abstract class MdmDataManager<TModel> : MdmDataManager
+        where TModel : IdentifiedData
+    {
+        /// <summary>
+        /// Persistence service
+        /// </summary>
+        protected IDataPersistenceService m_underlyingTypePersistence;
+
+        /// <summary>
+        /// Create a new manager base
+        /// </summary>
+        internal MdmDataManager(IDataPersistenceService underlyingDataPersistence)
+        {
+            if (underlyingDataPersistence == null)
+                throw new InvalidOperationException($"Type {typeof(TModel).FullName} does not have a persistence service registered");
+            this.m_underlyingTypePersistence = underlyingDataPersistence;
+        }
+
+        /// <summary>
+        /// Gets or creates a writable target object for the specified principal
+        /// </summary>
+        public abstract TModel GetLocalFor(Guid dataKey, IPrincipal principal);
+
+        /// <summary>
+        /// Determine if the record is a ROT
+        /// </summary>
+        public abstract bool IsRecordOfTruth(TModel data);
+
+        /// <summary>
+        /// Determine if the object is a master
+        /// </summary>
+        public abstract bool IsMaster(TModel data);
+
+        /// <summary>
+        /// Create local for the specified principal
+        /// </summary>
+        public abstract TModel CreateLocalFor(TModel masterRecord);
+
+        /// <summary>
+        /// Converts the <paramref name="local"/> to a ROT local
+        /// </summary>
+        public abstract TModel PromoteRecordOfTruth(TModel local);
+
+        /// <summary>
+        /// Get the master for the specified local
+        /// </summary>
+        public abstract IdentifiedData GetRawMaster(TModel local);
+
+        /// <summary>
+        /// Extracts the transactional components which might be in <paramref name="store"/> and return them
+        /// </summary>
+        public abstract IEnumerable<ISimpleAssociation> ExtractRelationships(TModel store);
+
+        /// <summary>
+        /// Validate the MDM state
+        /// </summary>
+        public abstract IEnumerable<DetectedIssue> ValidateMdmState(TModel data);
+
+        /// <summary>
+        /// Synthesize the query
+        /// </summary>
+        public abstract IEnumerable<IMdmMaster> MdmQuery(NameValueCollection query, NameValueCollection localQuery, Guid? queryId, int offset, int? count, out int totalResults, IEnumerable<ModelSort<TModel>> orderBy);
+
+        /// <summary>
+        /// Gets the raw object from the underlying persistence service identified by the key (whether it is MASTER ENTITY or LOCAL or SYNTH)
+        /// </summary>
+        public object GetRaw(Guid key)
+        {
+            return this.m_underlyingTypePersistence.Get(key);
+        }
+
+        /// <summary>
+        /// Ensures that <paramref name="principal"/> has access to a local <paramref name="data"/>
+        /// </summary>
+        public abstract bool IsOwner(TModel data, IPrincipal principal);
+
+        /// <summary>
+        /// Perform an MDM obsolete operation
+        /// </summary>
+        /// <returns>Whether the obsolete operation resulted in cascading obsolete to the master</returns>
+        public abstract IEnumerable<IdentifiedData> MdmTxObsolete(TModel data, IEnumerable<IdentifiedData> context);
+
+        /// <summary>
+        /// Create a transaction instructions which update <paramref name="data"/> to be the ROT
+        /// </summary>
+        public abstract IEnumerable<IdentifiedData> MdmTxSaveRecordOfTruth(TModel data, IEnumerable<IdentifiedData> context);
+
+        /// <summary>
+        /// Create transaction instructions which update the <paramref name="data"/> as a local
+        /// </summary>
+        public abstract IEnumerable<IdentifiedData> MdmTxSaveLocal(TModel data, IEnumerable<IdentifiedData> context);
+
+        /// <summary>
+        /// Create transaction instructions which update <paramref name="local"/> to match all masters
+        /// </summary>
+        public abstract IEnumerable<IdentifiedData> MdmTxMatchMasters(TModel local, IEnumerable<IdentifiedData> context);
 
         /// <summary>
         /// Create a new master for the local
--- SanteDB.Persistence.MDM/Services/Resources/MdmDataManagerFactory.cs
+++ SanteDB.Persistence.MDM/Services/Resources/MdmDataManagerFactory.cs
@@ -2,22 +2,23 @@
  * Copyright (C) 2021 - 2021, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
  * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
  * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: fyfej
  * Date: 2021-8-5
  */
+
 using SanteDB.Core.Configuration;
 using SanteDB.Core.Model;
 using SanteDB.Core.Model.Entities;
@@ -34,7 +35,6 @@ namespace SanteDB.Persistence.MDM.Services.Resources
     /// </summary>
     public static class MdmDataManagerFactory
     {
-
         // Created instances
         private static ConcurrentDictionary<Type, Object> m_createdInstances = new ConcurrentDictionary<Type, object>();
 
@@ -61,12 +61,20 @@ namespace SanteDB.Persistence.MDM.Services.Resources
         }
 
         /// <summary>
+        /// Create the specified data manager
+        /// </summary>
+        public static MdmDataManager GetDataManager(Type forType)
+        {
+            if (m_createdInstances.TryGetValue(forType, out object instance))
+                return (MdmDataManager)instance;
+            return null;
+        }
+
+        /// <summary>
         /// Register data manager
         /// </summary>
         internal static void RegisterDataManager(Type resourceType)
         {
-            
-
             if (typeof(Entity).IsAssignableFrom(resourceType))
             {
                 m_createdInstances.TryAdd(resourceType, Activator.CreateInstance(typeof(MdmEntityDataManager<>).MakeGenericType(resourceType)));
@@ -87,8 +95,6 @@ namespace SanteDB.Persistence.MDM.Services.Resources
                 return typeof(MdmEntityMerger<>).MakeGenericType(resourceType);
             }
             throw new InvalidOperationException("Cannot create MDM data merger for this type");
-
         }
-
     }
 }
