7638b6c350f39098c5860c89951845958cbf4d31
Justin Fyfe <justin@fyfesoftware.ca>
2022-01-26 10:54:15 -0500

    Updated to not use as-paralell

--- SanteDB.Persistence.MDM/Services/MdmDataManagementService.cs
+++ SanteDB.Persistence.MDM/Services/MdmDataManagementService.cs
@@ -352,7 +352,7 @@ namespace SanteDB.Persistence.MDM.Services
 
             // Results contain LOCAL records most likely
             // We have a resource type that matches
-            e.Results = e.Results.AsParallel().AsOrdered().Select((res) =>
+            e.Results = e.Results.Select((res) =>
             {
                 if (!this.m_configuration.ResourceTypes.Any(o => o.Type == res.GetType())) return res;
                 // Result is taggable and a tag exists for MDM
