7c673f89ac983c09192c4c2ba91b8ed33dd186f0
Justin Fyfe <justin@fyfesoftware.ca>
2022-09-08 11:14:48 -0400

    Updated license headers

--- SanteDB.Persistence.MDM/Model/IMdmRedirectedRelationship.cs
+++ SanteDB.Persistence.MDM/Model/IMdmRedirectedRelationship.cs
@@ -16,7 +16,7 @@
  * the License.
  * 
  * User: fyfej
- * Date: 2021-12-1
+ * Date: 2022-5-30
  */
 using SanteDB.Core.Model.Interfaces;
 using System;
--- SanteDB.Persistence.MDM/Services/MdmRecordMatchingService.cs
+++ SanteDB.Persistence.MDM/Services/MdmRecordMatchingService.cs
@@ -16,7 +16,7 @@
  * the License.
  * 
  * User: fyfej
- * Date: 2021-10-29
+ * Date: 2022-5-30
  */
 using SanteDB.Core;
 using SanteDB.Core.Model;
